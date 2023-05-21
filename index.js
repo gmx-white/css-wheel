5825f4b4ed53dfdbe76fd0a6f75fc014dc317f43
gmx <gao_mx2021@163.com>
2023-05-21 15:49:29 +0800

    modify: another way to achieve

--- index.js
+++ index.js
@@ -2,25 +2,24 @@
  * @Author: gmx
  * @Date: 2023-05-18 14:36:02
  * @LastEditors: gmx
- * @LastEditTime: 2023-05-20 22:57:14
+ * @LastEditTime: 2023-05-21 15:07:00
  * @Description: 
  * @Version: 1.0
  */
 function changeTime() {
   var time = new Date()
   var seconds = time.getSeconds()
-  if(seconds === 0) {
-    
-    doms.card1.innerHTML = seconds
+  doms.card1.innerHTML = seconds
     doms.card2.innerHTML = seconds
+  if(seconds === 0) {
     doms.card3.innerHTML = 59
     doms.card4.innerHTML = 59
   } else {
-    doms.card1.innerHTML = seconds
-    doms.card2.innerHTML = seconds
     doms.card3.innerHTML = seconds - 1 
     doms.card4.innerHTML = seconds - 1
   }
+  doms.card2.transform = "rotateX(0deg)"
+  doms.card3.transform = "rotateX(-180deg)"
   
 }
 var doms = {
@@ -29,4 +28,10 @@ var doms = {
   card3: document.querySelector('.card3'),
   card4: document.querySelector('.card4'),
 }
-var timer = setInterval(changeTime, 1000)
+doms.card2.ontransitionend = function (e) {
+  e.style.transform = "rotateX(180deg)"
+}
+doms.card3.ontransitionend = function (e) {
+  e.style.transform = "rotateX(0deg)"
+}
+var timer = setInterval(changeTime, 1000);
