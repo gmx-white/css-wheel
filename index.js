91f69bb28fa164013ddcfcc5ef164f44e0c6c3ef
gmx <gao_mx2021@163.com>
2023-05-21 22:01:11 +0800

    modify: 完善单项翻页

--- index.js
+++ index.js
@@ -2,24 +2,18 @@
  * @Author: gmx
  * @Date: 2023-05-18 14:36:02
  * @LastEditors: gmx
- * @LastEditTime: 2023-05-21 21:16:07
+ * @LastEditTime: 2023-05-21 21:55:36
  * @Description:
  * @Version: 1.0
  */
 function changeTime() {
   var now = new Date();
-  var nowTime = new Date(now.getTime() - 1000).getSeconds();
-  var nextTime = now.getSeconds();
-  doms.card1.innerHTML = nextTime;
-  doms.card2.innerHTML = nextTime;
-  doms.card3.innerHTML = nowTime;
-  doms.card4.innerHTML = nowTime;
-
-  if (!init) {
-    doms.card2.style.animationPlayState = "running";
-    doms.card3.style.animationPlayState = "running";
-    init = true;
-  }
+  var nowTime = now.getSeconds();
+  var nextTime = new Date(now.getTime() - 1000).getSeconds();
+  doms.card1.innerHTML = nowTime;
+  doms.card2.innerHTML = nowTime;
+  doms.card3.innerHTML = nextTime;
+  doms.card4.innerHTML = nextTime;
 }
 var doms = {
   card1: document.querySelector(".card1"),
@@ -27,6 +21,5 @@ var doms = {
   card3: document.querySelector(".card3"),
   card4: document.querySelector(".card4"),
 };
-var init = false;
 
-var timer = setInterval(changeTime, 1000);
+doms.card2.addEventListener('animationiteration', changeTime)
