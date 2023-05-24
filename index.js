b2f27532a9c1d62beded028bf9156e394d5812b5
gmx <gao_mx2021@163.com>
2023-05-24 20:33:03 +0800

    delete: 多余代码

--- index.js
+++ index.js
@@ -2,19 +2,7 @@
  * @Author: gmx
  * @Date: 2023-05-18 14:36:02
  * @LastEditors: gmx
- * @LastEditTime: 2023-05-18 15:52:35
+ * @LastEditTime: 2023-05-24 20:32:44
  * @Description: 
  * @Version: 1.0
  */
-// 平滑滚动
-var scrollBtn = document.querySelector('.top-button');
-scrollBtn.addEventListener('click', () => scrollTo({
-  top: 0,
-  left: 0,
-  behavior: "smooth"
-}));
-
-// 滚动小球
-var ballContainer = document.querySelector('.moving-ball')
-var ball = document.querySelector('.ball')
-ball.style.setProperty('--w', ballContainer.clientWidth + 'px');
